984316585ebe2561fcaad1729748bb5574bbac86
Noam <noamru22@gmail.com>
2020-10-09 14:14:53 +0100

    tryout css transition grooup

--- src/Components/Monitor/Monitor.js
+++ src/Components/Monitor/Monitor.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import Taskbar from "../Taskbar/Taskbar";
 import "./Monitor.css";
-import Test from "../Taskbar/Test"
 import SideMenu from "../Taskbar/SideMenu/SideMenu"
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 export default function Monitor() {
   let [openMenu, setOpenMenu] = useState(false);
@@ -17,7 +17,15 @@ export default function Monitor() {
     return (
       <div className="monitor">
         <Taskbar toggleStartMenu={toggleStartMenu} />
-        <SideMenu />
+        <TransitionGroup className="tryout">
+          <CSSTransition
+            // in={true}
+            timeout={1000}
+            classNames="fade"
+            key={1}>
+            <SideMenu />
+          </CSSTransition>
+        </TransitionGroup>
       </div>
     )
   } else {
